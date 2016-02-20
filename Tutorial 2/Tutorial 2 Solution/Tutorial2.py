72a57a0e2c88996a1bb6d53f9c28c7eb758d6c2d
chadmadna <chadmadna@gmail.com>
2016-02-21 00:03:16 +0700

    Added tutorial 2 .py files and test cases

---
+++ Tutorial 2/Tutorial 2 Solution/Tutorial2.py
@@ -0,0 +1,119 @@
+# Data Structures & Algorithms
+# Tutorial 2: Abstract Data Type
+# Name: TODO Write your name here
+# NPM: TODO Write your NPM here
+
+
+class Track:
+    def __init__(self, title, artist, genre, is_fav):
+        self.title = title
+        self.artist = artist
+        self.genre = genre
+        self.is_fav = is_fav
+
+
+class Playlist:
+    def __init__(self, queue=None):
+        if not queue:
+            queue = []
+        self.queue = queue
+
+    def add(self, track=None):
+        # TODO Implement me!
+        if track in self.queue:
+            print("'{} - {}' already in playlist.".format(track.artist, track.title))
+        self.queue.append(track)
+        print("'{} - {}' added to playlist.".format(track.artist, track.title))
+
+    def delete_title(self, title):
+        # TODO Implement me!
+        for track in self.queue:
+            if track.title == title:
+                print("'{} - {}' deleted from playlist.".format(track.artist, track.title))
+                del track
+        else: print("'{}' not found in playlist.".format(title))
+
+    def delete_position(self, pos):
+        # TODO Implement me!
+        if pos in range(1, len(self.queue)+1):
+            del self.queue[pos-1]
+            print("Track {} deleted from playlist.".format(pos))
+        else: print("Invalid track position.")
+
+    def move(self, n, m):
+        # TODO Implement me!
+        if n in range(1, len(self.queue)+1) and m in range(1, len(self.queue)+1):
+            self.queue.insert(m, self.queue.pop(n))
+            print("Track {} moved to position {}.".format(n, m))
+        else: print("Invalid track position.")
+
+    def count_genre(self, genre):
+        # TODO Implement me!
+        count = 0
+        for track in self.queue:
+            if track.genre == genre:
+                count += 1
+        print("Number of {} tracks: {}".format(genre, count))
+
+    def count_favourite(self):
+        # TODO Implement me!
+        count = 0
+        for track in self.queue:
+            if track.is_fav is True:
+                count += 1
+        print("Number of favourited tracks: {}".format(count))
+
+    def print_playlist(self):
+        # TODO Implement me!
+        for track in self.queue:
+            print("{} - {}".format(track.artist, track.title))
+
+
+def main():
+    """
+    Main function to run the program. Instantiates a Playlist object,
+    opens test case files and parses each line as an operation on the
+    playlist object.
+    """
+
+    for i in range(1, 4):
+        print("\n[SAMPLE INPUT {}]".format(i))
+
+        playlist = Playlist()
+
+        filename = "testinput{}.txt".format(i)
+
+        with open(filename, 'r') as testfile:
+            operation_list = testfile.read().splitlines()
+
+        for line in operation_list:
+            operation = line.split(',')
+            op_type = operation[0]
+            if op_type == 'ADD':
+                title, artist, genre, is_fav = operation[1:]
+                if is_fav == 'True':
+                    is_fav = True
+                else:
+                    is_fav = False
+                playlist.add(Track(title, artist, genre, is_fav))
+            elif op_type == 'DELTITLE':
+                title = operation[1]
+                playlist.delete_title(title)
+            elif op_type == 'DELPOS':
+                position = int(operation[1])
+                playlist.delete_position(position)
+            elif op_type == 'MOVE':
+                old_pos, new_pos = int(operation[1]), int(operation[2])
+                playlist.move(old_pos, new_pos)
+            elif op_type == 'COUNTGENRE':
+                genre = operation[1]
+                playlist.count_genre(genre)
+            elif op_type == 'COUNTFAV':
+                playlist.count_favourite()
+            elif op_type == 'PRINT':
+                playlist.print_playlist()
+
+
+# Runs the program if executed via command prompt
+if __name__ == '__main__':
+    main()
